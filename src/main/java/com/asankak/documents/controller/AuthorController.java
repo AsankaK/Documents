d6a5adf9cb0f94b54446f73afba11b5e016ec24e
AsankaK <saradeasa@gmail.com>
2024-08-29 21:53:42 +0200

    adding gitignore
    Added swagger documentation

--- src/main/java/com/asankak/documents/controller/AuthorController.java
+++ src/main/java/com/asankak/documents/controller/AuthorController.java
@@ -14,11 +14,18 @@ import com.asankak.documents.dto.AuthorList;
 import com.asankak.documents.model.Author;
 import com.asankak.documents.service.AuthorService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/author")
+@ApiResponses(value = { @ApiResponse(responseCode = "500", description="server error", content=@Content(mediaType = "application/json")),
+		@ApiResponse(responseCode = "400", description="client error", content=@Content(mediaType = "application/json"))
+})
 public class AuthorController {
 
 	private final AuthorService service;
@@ -28,22 +35,32 @@ public class AuthorController {
 		this.service = service;
 	}
 
+	@Operation(summary = "Gets all authors", description = "If no authors exist, empty list is returned")
+	@ApiResponse(responseCode = "200", description="returns a list of all authors", content=@Content(mediaType = "application/json"))
 	@GetMapping
 	public ResponseEntity<AuthorList> getAuthors() {
 		return ResponseEntity.ok().body(new AuthorList(service.getAuthors()));
 	}
 
+	@Operation(summary = "Creates a new Author", description = "created Author is returned")
+	@ApiResponse(responseCode = "200", description="returns newly created Author", content=@Content(mediaType = "application/json"))
 	@PostMapping
 	public ResponseEntity<Author> addAuthor(@Validated @RequestBody Author author) {
 		return ResponseEntity.ok().body(service.addAuthor(author));
 	}
 
+	@Operation(summary = "Updates existing author", description = "If an author exists, with given id, all the non-null fields are updated."+
+			"If no author exists with given id, error response is returned.")
+	@ApiResponse(responseCode = "200", description="returns updated Author", content=@Content(mediaType = "application/json"))
 	@PatchMapping
 	public ResponseEntity<Author> updateAuthor(@Validated @RequestBody Author author) {
 
 		return ResponseEntity.ok().body(service.updateAuthor(author));
 	}
 
+	@Operation(summary = "Deletes existing author", description = "If an author exists with specified id, it will be deleted."+
+			"If no author exists with specified id, error response is returned.")
+	@ApiResponse(responseCode = "200", description="indicates successful deletion of Author", content=@Content(mediaType = "application/json"))
 	@DeleteMapping
 	public ResponseEntity<Void> deleteAuthor(@RequestBody Author author) {
 		service.deleteAuthor(author);
--- src/main/java/com/asankak/documents/controller/DocumentController.java
+++ src/main/java/com/asankak/documents/controller/DocumentController.java
@@ -14,11 +14,18 @@ import com.asankak.documents.dto.DocumentList;
 import com.asankak.documents.model.Document;
 import com.asankak.documents.service.DocumentService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/document")
 @Slf4j
+@ApiResponses(value = { @ApiResponse(responseCode = "500", description="server error", content=@Content(mediaType = "application/json")),
+		@ApiResponse(responseCode = "400", description="client error", content=@Content(mediaType = "application/json"))
+})
 public class DocumentController {
 
 	private final DocumentService service;
@@ -28,21 +35,31 @@ public class DocumentController {
 		this.service = service;
 	}
 
+	@Operation(summary = "Gets all documents", description = "If no documents exist, empty list is returned")
+	@ApiResponse(responseCode = "200", description="returns a list of all documents", content=@Content(mediaType = "application/json"))
 	@GetMapping
 	public ResponseEntity<DocumentList> getDocuments() {
 		return ResponseEntity.ok().body(new DocumentList(service.getDocuments()));
 	}
 
+	@Operation(summary = "creates a new Document", description = "created Document is returned.")
+	@ApiResponse(responseCode = "200", description="returns the created document", content=@Content(mediaType = "application/json"))
 	@PostMapping
 	public ResponseEntity<Document> addDocument(@Validated @RequestBody Document document) {
 		return ResponseEntity.ok().body(service.addDocument(document));
 	}
 
+	@Operation(summary = "Updates existing Document", description = "If a document exists with specified id, all the non-null fields are updated."+
+			"If no document exists with specified id, error response is returned.")
+	@ApiResponse(responseCode = "200", description="returns updated document", content=@Content(mediaType = "application/json"))
 	@PatchMapping
 	public ResponseEntity<Document> updateDocument(@Validated @RequestBody Document document) {
 		return ResponseEntity.ok().body(service.updateDocument(document));
 	}
 
+	@Operation(summary = "Deletes existing Document", description = "If a Document exists with specified id, it will be deleted."+
+			"If no Document exists with specified id, error response is returned.")
+	@ApiResponse(responseCode = "200", description="indicates successful deletion of document", content=@Content(mediaType = "application/json"))
 	@DeleteMapping
 	public ResponseEntity<Void> deleteDocument(@Validated @RequestBody Document document) {
 		service.deleteDocument(document);
